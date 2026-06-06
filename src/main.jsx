6bae9966874fe0329d4295c75c98e4e235997d5c
rodzdev1971 <rodzrx@gmail.com>
2026-06-06 14:57:04 -0400

    BrowserRouter added it the base aeviora

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/pages">
+    <BrowserRouter basename="/aeviora">
       <App />
     </BrowserRouter>
   </React.StrictMode>
