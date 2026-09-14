b36617c41983fa87b3e265f4ce1d971b05a6682e
rodzdev1971 <rodzdev1971@gmail.com>
2026-09-14 13:24:21 -0400

    change bases to '/'

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="">
-      <App />
+    <BrowserRouter basename="/">
+       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
